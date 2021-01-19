0cd9f939d7bb0c5066707b2191549e548b468e55
Kenneth Okalang <68539411+Atamakit@users.noreply.github.com>
2021-01-19 16:52:29 +0100

    Add project files.

---
+++ Restaurang Anställda Registret/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Restaurang_Anställda_Registret
+{
+       public class anställda
+    {
+        public string namn;
+        public string lön;
+
+        public void Registret()
+        {
+            Console.WriteLine("Registret:" + namn + " " + lön);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kenneth = new anställda();
+            kenneth.namn = "Kenneth ";
+            kenneth.lön = "25,000";
+            kenneth.Registret();
+
+        }
+    }
+}
